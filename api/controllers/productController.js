57549f94a09148ce98b5fe975cd204651014a2b5
Ramon Raupp <ramonrooliveira@gmail.com>
2020-04-19 16:03:22 -0300

    update catch blocks

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -5,40 +5,29 @@ var mongoose = require('mongoose'),
 
 exports.list_all_products = function(req, res) {
   Product.find({}, function(err, product) {
-    // if (err)
-    //   res.send(err);
+    res.json(product);
+  }).catch( err => {throw new Error(err);} );
+};
+
+exports.create_a_product = function(req, res) {
+  var newProduct = new Product(req.body);
+  newProduct.save(function(err, product) {
     res.json(product);
   }).catch( err => {throw new Error(err);} );
 };
 
 
-
-
-exports.create_a_product = function(req, res) {
-  var newProduct = new Product(req.body);
-  newProduct.save(function(err, product) {
-    if (err)
-      res.send(err);
-    res.json(product);
-  });
-};
-
-
 exports.read_a_product = function(req, res) {
   Product.findById(req.params.productId, function(err, product) {
-    if (err)
-      res.send(err);
     res.json(product);
-  });
+  }).catch( err => {throw new Error(err);} );
 };
 
 
 exports.update_a_product = function(req, res) {
   Product.findOneAndUpdate({_id: req.params.productId}, req.body, {new: true}, function(err, product) {
-    if (err)
-      res.send(err);
     res.json(product);
-  });
+  }).catch( err => {throw new Error(err);} );
 };
 
 
@@ -46,8 +35,6 @@ exports.delete_a_product = function(req, res) {
   Product.remove({
     _id: req.params.productId
   }, function(err, product) {
-    if (err)
-      res.send(err);
     res.json({ message: `Product successfully deleted` });
-  });
+  }).catch( err => {throw new Error(err);} );
 };
